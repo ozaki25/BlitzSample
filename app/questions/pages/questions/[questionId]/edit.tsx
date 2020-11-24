3baeba3b86441ab1bfe6c3583e2e97889d09c80b
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-11-24 11:45:25 +0900

    modify update question

--- app/questions/pages/questions/[questionId]/edit.tsx
+++ app/questions/pages/questions/[questionId]/edit.tsx
@@ -18,11 +18,20 @@ export const EditQuestion = () => {
 
       <QuestionForm
         initialValues={question}
-        onSubmit={async () => {
+        onSubmit={async (event) => {
           try {
             const updated = await updateQuestionMutation({
               where: { id: question.id },
-              data: { text: "Do you really love Blitz?" },
+              data: {
+                text: event.target[0].value,
+                choices: {
+                  create: [
+                    { text: event.target[1].value },
+                    { text: event.target[2].value },
+                    { text: event.target[3].value },
+                  ],
+                },
+              },
             })
             await setQueryData(updated)
             alert("Success!" + JSON.stringify(updated))
